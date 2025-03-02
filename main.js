2c9be4308af64b236d7d73ceab820f4e9353d3c2
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-03-02 14:31:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// CONTACT PAGE
+function namechange() {
+	const fname = document.getElementById('fname');
+	fname.value = fname.value.toUpperCase();
+}
 
+
+function changeSbmtColor() {
+	const subfeed = document.getElementById('subfeed');
+	subfeed.style.color = "green";
+	subfeed.style.background = "white";
+}
+
+function resetSbmtColor() {
+	const subfeed = document.getElementById('subfeed');
+	subfeed.style.color = "white";
+	subfeed.style.background = "green";
+}
