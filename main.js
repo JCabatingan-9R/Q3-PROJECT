a6170af93e3076ce3a2317ec5860d35bc7d29a9f
JCabatingan-9R <cabatingan.jc@obmontessori.edu.ph>
2025-03-07 13:41:07 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+//HOME PAGE
+function changeRegColor() {
+	const reg = document.getElementById('reg');
+	reg.style.color = "green";
+	reg.style.background = "white";
+}
+
+function resetRegColor() {
+	const reg = document.getElementById('reg');
+	reg.style.color = "white";
+	reg.style.background = "green";
+}
+
+function changeDiscColor() {
+	const disc = document.getElementById('disc');
+	disc.style.color = "green";
+	disc.style.background = "white";
+}
+
+function resetDiscColor() {
+	const disc = document.getElementById('disc');
+	disc.style.color = "white";
+	disc.style.background = "green";
+}
+
+function regular() {
+	var origin = document.getElementById('origin').value;
+	var destination = document.getElementById('destination').value;
+	var price = destination - origin;
+	var price.value = price.value.toFixed(2);
+	document.getElementById('ticket').innerHTML = price.value;
+}
+
+function discount() {
+	var origin = document.getElementById('origin').value;
+	var destination = document.getElementById('destination').value;
+	var price = (destination - origin) * .2;
+	var price.value = price.value.toFixed(2);
+	document.getElementById('ticket').innerHTML = price.value;
+}
+
 // CONTACT PAGE
 function namechange() {
 	const fname = document.getElementById('fname');
